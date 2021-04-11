66f81bf8296903b61cd24cdca0d45e5961dc33d1
esfredson <federicofrasquet@gmail.com>
2021-04-11 17:44:33 +0200

    Este cambio es demo para azure devops ci

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -17,6 +17,8 @@ namespace TiendaServicios.Api.Libro.Tests
     {
         private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
+            // Este método es para llenar data con genfu
+
             A.Configure<LibreriaMaterial>()
                 .Fill(x => x.Titulo).AsArticleTitle()
                 .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
